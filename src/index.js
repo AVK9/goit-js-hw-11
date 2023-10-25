c1a282e242ed04f0cf45bc7e7457835b45c35ae4
AlexKat <katsubo79@gmail.com>
2023-10-26 02:36:39 +0300

    1

--- src/index.js
+++ src/index.js
@@ -5,33 +5,83 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 const API_KEY = "40254095-fb7e3bf791467f50a6328bb1e";
 axios.defaults.baseURL = 'https://pixabay.com/api';
-axios.defaults.headers.common["x-api-key"] = `${API_KEY}`;
 
 const refs = {
-    form: document.querySelector('#search-form"'),
+    form: document.querySelector('#search-form'),  
     gallery: document.querySelector('.gallery'),
     loadmoreBtn: document.querySelector('.load-more'),
 }
+const { searchQuery} = refs.form.elements;
 
-let inputDataUser;
+// let inputDataUser;
 
-refs.form.addEventListener('submit', searchQuery)
-function searchQuery(e) {
+refs.form.addEventListener('submit', onSearchQuery)
+function onSearchQuery(e) {
     e.preventDefault();
-    console.log(e.value);
+    pixabayBase(searchQuery.value);
+    
 }
 
 function pixabayBase (inputDataUser) { 
    return axios.get('/', {
-        params: {
+       params: {
+           key: API_KEY,
            q: inputDataUser,
-           image_type: "photo",
+           image_type: "photo", 
            orientation: "horizontal",
            safesearch: "true",
         }
    })
-        .then(response => {
-      console.log(response.data);
+       .then(resp => {
+            refs.gallery.innerHTML =  creatMarkup(resp.data.hits);
+           console.log(resp.data.hits.length);
+           if (!resp.data.hits.length) {
+               Notiflix.Notify.failure(`Sorry, there are no images matching your search query. Please try again.`);
+            }
     })
 
 };
+
+function creatMarkup(arr) {
+   // console.log(arr);
+    return arr.map((
+     {
+ webformatURL,
+largeImageURL,
+tags,
+likes,
+views,
+comments,
+downloads
+}
+     ) =>
+     `
+
+   
+<div class="photo-card">
+<a class="gallery__link" href="${largeImageURL}">
+  <img class="images" src="${webformatURL}" alt="${tags}" loading="lazy" />
+  
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b> <br>${likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b> <br>${views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b> <br>${comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b> <br>${downloads}
+    </p>
+  </div>
+  </a>
+</div>
+        `).join("");
+
+}
+    const lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: "alt",
+        captionDelay: 250,
+});
