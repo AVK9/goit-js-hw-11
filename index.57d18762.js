205ccf7db175b8858e3b54358d6f03dc4efd3736
AVK9 <64771146+AVK9@users.noreply.github.com>
2023-10-31 09:40:50 +0000

    Deploying to gh-pages from @ AVK9/goit-js-hw-11@560a8643e611b16ee8725267ec1d3f805e4f4f00 🚀

---
+++ index.57d18762.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".go-top");window.addEventListener("scroll",(function(){var o=window.pageYOffset,t=document.documentElement.clientHeight;o>t?e.classList.add("go-top--show"):e.classList.remove("go-top--show")})),e.addEventListener("click",(function e(){window.pageYOffset>0&&(window.scrollBy(0,-25),setTimeout(e,0))}))}();
+//# sourceMappingURL=index.57d18762.js.map
---
+++ index.da664919.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".go-top");window.addEventListener("scroll",(function(){const o=window.pageYOffset,t=document.documentElement.clientHeight;o>t?e.classList.add("go-top--show"):e.classList.remove("go-top--show")})),e.addEventListener("click",(function e(){window.pageYOffset>0&&(window.scrollBy(0,-25),setTimeout(e,0))}));
+//# sourceMappingURL=index.da664919.js.map
